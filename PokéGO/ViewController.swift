5b8238afd1f8bbbec7204e07abebf38fc83b8d6d
Mayank Mehta <mayankmehta@mayanks-macbook-air.local>
2017-04-06 10:18:22 +0530

    Initial Commit

---
+++ PokéGO/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PokéGO
+//
+//  Created by Mayank Mehta on 06/04/17.
+//  Copyright © 2017 Mayank Mehta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
