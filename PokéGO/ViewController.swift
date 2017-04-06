32850a40e1f764a6f38147a9113edf0d1653f593
Mayank Mehta <mayankmehta@Mayanks-MacBook-Air.local>
2017-04-06 11:10:22 +0530

    set region and location settings

--- PokéGO/ViewController.swift
+++ PokéGO/ViewController.swift
@@ -7,13 +7,40 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.manager.delegate = self
+        
+        // Authorization status and location settings
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
+            
+            //shows current location blue point
+            self.mapView.showsUserLocation = true
+            
+            self.manager.startUpdatingLocation()
+            
+        }else{
+            self.manager.requestWhenInUseAuthorization()
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //set region
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let region = MKCoordinateRegionMakeWithDistance(self.manager.location!.coordinate, 400, 400)
+        
+        self.mapView.setRegion(region, animated: true)
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
