53203f5c440ce36f4ca1039542155a286a00d77d
Mayank Mehta <mayankmehta@Mayanks-MacBook-Air.local>
2017-04-07 12:45:43 +0530

    added background node to battlescene

--- PokéGO/BattleScene.swift
+++ PokéGO/BattleScene.swift
@@ -14,9 +14,19 @@ class BattleScene : SKScene, SKPhysicsContactDelegate {
     var pokemon : Pokemon!
     
     override func didMove(to view: SKView) {
-        print("CatchEM")
+        //print("CatchEM")
+        //print(pokemon.name)
         
-        print(pokemon.name)
+        // Background Code
+        let battleBg = SKSpriteNode(imageNamed: "bggg")
+        battleBg.size = self.size
+        battleBg.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        battleBg.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        battleBg.zPosition = -1
+        self.addChild(battleBg)
+        
+        
+        
     }
     
 }
