a63d8c831688641af1d383563b4e5647565e4223
Mayank Mehta <mayankmehta@Mayanks-MacBook-Air.local>
2017-04-07 15:06:20 +0530

    added pokemon-action and pokeball code

--- PokéGO/BattleScene.swift
+++ PokéGO/BattleScene.swift
@@ -13,6 +13,14 @@ class BattleScene : SKScene, SKPhysicsContactDelegate {
     
     var pokemon : Pokemon!
     
+    //creating spirtes
+    var pokemonSprite : SKSpriteNode!
+    var pokeballSprite : SKSpriteNode!
+    
+    //define constants
+    let kPokemonSize = CGSize(width: 140, height: 140)
+    let kPokeballSize = CGSize(width: 60, height: 60)
+    
     override func didMove(to view: SKView) {
         //print("CatchEM")
         //print(pokemon.name)
@@ -25,6 +33,36 @@ class BattleScene : SKScene, SKPhysicsContactDelegate {
         battleBg.zPosition = -1
         self.addChild(battleBg)
         
+        //call pokemon and pokeball func
+        self.perform(#selector(setupPokemon), with: nil, afterDelay: 2.0)
+        self.perform(#selector(setupPokeball), with: nil, afterDelay: 2.0)
+        
+    }
+    
+    //Pokemon code
+    func setupPokemon() {
+        self.pokemonSprite = SKSpriteNode(imageNamed: pokemon.imageFileName!)
+        self.pokemonSprite.size = kPokemonSize
+        self.pokemonSprite.position = CGPoint(x: self.size.width/2, y: self.size.height/2)
+        self.pokemonSprite.zPosition = 1
+        
+        //movements
+        let moveRight = SKAction.moveBy(x: 100, y: 0, duration: 3.0)
+        let sequence = SKAction.sequence([moveRight, moveRight.reversed(), moveRight.reversed(), moveRight])
+        //keep the pokemon moving forever
+        self.pokemonSprite.run(SKAction.repeatForever(sequence))
+        
+        self.addChild(pokemonSprite)
+        
+    }
+    
+    //Pokeball code
+    func setupPokeball() {
+        self.pokeballSprite = SKSpriteNode(imageNamed: "pokeball")
+        self.pokeballSprite.size = kPokeballSize
+        self.pokeballSprite.position = CGPoint(x: self.size.width/2, y: 100)
+        self.pokeballSprite.zPosition = 1
+        self.addChild(pokeballSprite)
         
         
     }
